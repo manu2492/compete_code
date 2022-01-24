e88ffcab0d3f3eccb4c636f6fd509647c28a7bc4
Emanuel Schemberger <emanueldev24@gmail.com>
2022-01-24 16:00:28 -0300

    change 2

---
+++ 10-codechef/finding_squareroots.py
@@ -0,0 +1,23 @@
+#the is the number of times that the program will ask you for a number
+t = int(input())
+
+#the program will ask t times
+for i in range(t):
+    #the program will ask you for une number
+    n = int(input())
+    #print the square root of n
+    print(int(n**0.5))
+
+#the input: 5
+
+#the output:
+# 4
+# 2
+# 9
+# 3
+# 4
+# 2
+# 9
+# 3
+# 4
+# 2
---
+++ 10-codechef/turbosort.py
@@ -0,0 +1,26 @@
+#t = input()
+#t = list(t)
+#
+#t.sort()
+#
+#def iterar():
+#    
+#
+#    for i in t:
+#        print(i)
+#
+#iterar()
+
+
+
+
+#second solution
+lis=[]
+t=int(input())
+while t>0:
+    n=int(input())
+    lis.append(n)
+    t-=1
+lis.sort()
+for i in range(0,len(lis)):
+    print(lis[i])
